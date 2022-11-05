5a9a0933ddf26beb00b4eb1a6dd06ee5e4ccaef3
codereyasin <eyasindevofficial@gmail.com>
2022-11-05 21:04:55 +0600

    commit

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     // Mobile Screen
     <div className="sm:hidden absolute right-0 top-[77vh] z-10 items-center pr-5 flex justify-end">
       <h1 className="flex justify-center items-center gap-1 border px-3 py-2 rounded-2xl outline-none text-black font-semibold">
-        <select name="Nav border-none outline-none" >
+        <select name="Nav" className="outline-none border-none" >
           <option
             className="level-0 "
             value="breads-sweets"
