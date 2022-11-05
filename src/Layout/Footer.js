c56d5ebc62c1706e0a759bc08a49b81640f8b61e
codereyasin <eyasindevofficial@gmail.com>
2022-11-05 20:55:06 +0600

    commit

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -6,35 +6,35 @@ const Footer = () => {
     // Mobile Screen
     <div className="sm:hidden items-center pr-5 flex justify-end">
       <h1 className="flex justify-center items-center gap-1 border px-3 py-2 rounded-2xl outline-none text-black font-semibold">
-        <select name="Nav">
+        <select name="Nav border-none outline-none" >
           <option
-            className="level-0 border-none outline-none"
+            className="level-0 "
             value="breads-sweets"
           >
           <Link href="/"> Home <AiOutlineDown /></Link>
           </option>
           <option
-            className="level-0 border-none outline-none"
+            className="level-0 "
             value="breads-sweets"
           >
            <a href='/About'> About us <AiOutlineDown /></a>
           </option>
           <option
-            className="level-0 border-none outline-none"
+            className="level-0 "
             value="breads-sweets"
           >
             {" "}
             Our vision <AiOutlineDown />
           </option>
           <option
-            className="level-0 border-none outline-none"
+            className="level-0 "
             value="breads-sweets"
           >
             {" "}
             Faq <AiOutlineDown />
           </option>
           <option
-            className="level-0 border-none outline-none"
+            className="level-0 "
             value="breads-sweets"
           >
             {" "}
