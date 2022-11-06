2054ffa0b7685a559ac3436822f15328c84fa5c0
jsdevraju <islamrajulearn.dev@gmail.com>
2022-11-06 18:34:34 +0600

    added route property

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,55 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineDown, AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Footer = () => {
+  const navigate = useNavigate();
+  function handleRedirect(e) {
+    if (e.target.value === "ourvision") navigate("ourvision");
+    if (e.target.value === "home") navigate("/");
+  }
+
   return (
     <>
-    {/* Mobile Screen */}
-    <div className="sm:hidden top-[77vh] z-10 items-center pt-10 pr-5 flex justify-between">
-      <div className="flex justify-end absolute right-2 items-center gap-1 border px-3 py-2 rounded-2xl outline-none text-black font-semibold">
-        <select name="Nav" className="outline-none border-none custom-select w-[200px]" >
-          <option
-            className="level-0 bg-transparent border-none outline-none "
-            value="breads-sweets"
+      {/* Mobile Screen */}
+      <div className="sm:hidden top-[77vh] z-10 items-center pt-10 pr-5 flex justify-between">
+        <div className="flex justify-end absolute right-2 items-center gap-1 border px-3 py-2 rounded-2xl outline-none text-black font-semibold">
+          <select
+            onChange={handleRedirect}
+            name="Nav"
+            className="outline-none border-none custom-select w-[200px]"
           >
-          <Link to="/"> Home <AiOutlineDown /></Link>
-          </option>
-          <option
-            className="level-0 "
-            value="breads-sweets"
-          >
-          <Link to="/about">
-          <a className="hover:text-gray-400"> About us <AiOutlineDown /></a>
-          </Link>
-          </option>
-          <option
-            className="level-0"
-            value="level-1"
-          >
-            {" "}
-            Our vision <AiOutlineDown />
-          </option>
-          <option
-            className="level-0 "
-            value="level-2"
-          >
-            {" "}
-            Faq <AiOutlineDown />
-          </option>
-          <option
-            className="level-0 "
-            value="level-3"
-          >
-            {" "}
-            Conact us <AiOutlineDown />
-          </option>
-        </select>
+            <option
+              className="level-0 bg-transparent border-none outline-none "
+              value="home"
+            >
+              <Link to="/">
+                {" "}
+                Home <AiOutlineDown />
+              </Link>
+            </option>
+            <option className="level-0 " value="about">
+              <Link to="/about">
+                <a className="hover:text-gray-400">
+                  {" "}
+                  About us <AiOutlineDown />
+                </a>
+              </Link>
+            </option>
+            <option className="level-0" value="ourvision">
+              {" "}
+              Our vision <AiOutlineDown />
+            </option>
+            <option className="level-0 " value="faq">
+              {" "}
+              Faq <AiOutlineDown />
+            </option>
+            <option className="level-0 " value="contactus">
+              {" "}
+              Conact us <AiOutlineDown />
+            </option>
+          </select>
+        </div>
+        <div className="flex justify-start items-start gap-2 p-l left-2 absolute">
+          <AiOutlineLeft className="border rounded-md px-1 z-10" size={30} />
+          <AiOutlineRight className="border rounded-md px-1 z-10" size={30} />
+        </div>
       </div>
-      <div className="flex justify-start items-start gap-2 p-l left-2 absolute">
-      <AiOutlineLeft className="border rounded-md px-1 z-10" size={30}/>
-      <AiOutlineRight className="border rounded-md px-1 z-10" size={30}/>
-      </div>
-    </div>
     </>
   );
 };
